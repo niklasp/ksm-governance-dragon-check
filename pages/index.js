d7990a5c9cdf15ece1cab95539dddc674e2cba86
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-07-05 19:52:52 +0200

    wip

--- pages/index.js
+++ pages/index.js
@@ -38,13 +38,13 @@ export default function Home() {
         <meta property="og:url" content="https://metatags.io/" />
         <meta property="og:title" content="Can my dragon Grow? - Kusama Governance Rewards - Unofficial" />
         <meta property="og:description" content="here you can check if you meet the requirements for your blue dragon to grow." />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="https://ksm-governance-dragon-check.vercel.app/meta.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://twitter.com/niftesty" />
         <meta property="twitter:title" content="Can my dragon Grow? - Kusama Governance Rewards - Unofficial" />
         <meta property="twitter:description" content="here you can check if you meet the requirements for your blue dragon to grow." />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="https://ksm-governance-dragon-check.vercel.app/meta.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
