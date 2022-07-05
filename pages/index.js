10fe22285353ba83794d52ebd737acbbfb7f2fde
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-07-05 18:06:38 +0200

    a

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Dragon Grow Checker - Kusama Governance Rewards</title>
+        <title>Can my dragon Grow? - Kusama Governance Rewards - Unofficial</title>
+        <meta name="title" content="Can my dragon Grow? - Kusama Governance Rewards - Unofficial" />
+        <meta name="description" content="here you can check if you meet the requirements for your blue dragon to grow." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Can my dragon Grow? - Kusama Governance Rewards - Unofficial" />
+        <meta property="og:description" content="here you can check if you meet the requirements for your blue dragon to grow." />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://twitter.com/niftesty" />
+        <meta property="twitter:title" content="Can my dragon Grow? - Kusama Governance Rewards - Unofficial" />
+        <meta property="twitter:description" content="here you can check if you meet the requirements for your blue dragon to grow." />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
